7c385940922f1560b59ab19dee02aa90364e79da
acmestones <marblehouse@gmail.com>
2025-11-15 17:14:28 +0530

    Create get_album_dirs.php

---
+++ get_album_dirs.php
@@ -0,0 +1,26 @@
+<?php
+define('PHPWG_ROOT_PATH', './');
+include_once(PHPWG_ROOT_PATH.'include/common.inc.php');
+
+// Check if user is admin
+if (!is_admin()) {
+    echo json_encode(['error' => 'Unauthorized']);
+    exit;
+}
+
+$query = 'SELECT id, name, dir FROM '.CATEGORIES_TABLE.' ORDER BY id;';
+$result = pwg_query($query);
+
+$albums = [];
+while ($row = pwg_db_fetch_assoc($result)) {
+    $albums[] = [
+        'id' => $row['id'],
+        'name' => $row['name'],
+        'dir' => $row['dir'],
+        'isPhysical' => !empty($row['dir'])
+    ];
+}
+
+header('Content-Type: application/json');
+echo json_encode(['albums' => $albums]);
+?>
