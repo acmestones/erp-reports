2eb13501125576ef3dbe8c5551125482dd430a85
acmestones <marblehouse@gmail.com>
2025-11-24 18:31:32 +0530

    Update fetch_plytix_data.php

--- fetch_plytix_data.php
+++ fetch_plytix_data.php
@@ -1,4 +1,9 @@
 <?php
+@unlink($cacheFile);
+@unlink($timestampFile);
+@unlink($callCountFile);
+
+
 // Set max execution time and memory to handle large data safely
 set_time_limit(300);
 ini_set('memory_limit', '256M');
