cec369f7d751fdb9f989f38d42128fa27de8bca4
acmestones <marblehouse@gmail.com>
2025-11-24 18:10:36 +0530

    Update fetch_plytix_data.php

--- fetch_plytix_data.php
+++ fetch_plytix_data.php
@@ -2,6 +2,9 @@
 ini_set('log_errors', 'On');
 ini_set('error_log', __DIR__ . '/php-error.log');
 error_log("PHP script executed at " . date('Y-m-d H:i:s'));
+error_log("Cache file path: $cacheFile");
+error_log("Timestamp file path: $timestampFile");
+
 
 set_time_limit(300); // 5 minutes max execution time
 ini_set('memory_limit', '256M'); // Increase PHP memory limit
