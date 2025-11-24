6d11ce91a776fd76f063c9af3fea246877dfb3d4
acmestones <marblehouse@gmail.com>
2025-11-24 17:09:03 +0530

    Update fetch_plytix_data.php

--- fetch_plytix_data.php
+++ fetch_plytix_data.php
@@ -20,8 +20,8 @@ if (file_exists($cacheFile) && ($currentTime - filemtime($cacheFile) < $cacheTim
 }
 
 $callCount = 0;
-$apiKey = "YOUR_API_KEY_HERE";
-$apiPassword = "YOUR_API_PASSWORD_HERE";
+$apiKey = "DQ1TBOXSRPE196ER4018";
+$apiPassword = "0&0eqfaSvwb1iGdHRWL0nJZ9heuDJA3y@J;37S8z";
 
 // Authenticate
 $authCh = curl_init();
