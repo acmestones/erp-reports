0cb78a89db21db79ba718eea9b550a5a59bc96fb
acmestones <marblehouse@gmail.com>
2025-11-24 14:59:06 +0530

    Update fetch_plytix_data.php

--- fetch_plytix_data.php
+++ fetch_plytix_data.php
@@ -5,7 +5,7 @@ header('Content-Type: application/json');
 $apiKey = "DQ1TBOXSRPE196ER4018";
 $apiPassword = "0&0eqfaSvwb1iGdHRWL0nJZ9heuDJA3y@J;37S8z";
 
-// Step 1: Get access token with POST request
+// Step 1: Get access token
 $authCh = curl_init();
 curl_setopt($authCh, CURLOPT_URL, "https://auth.plytix.com/auth/api/get-token");
 curl_setopt($authCh, CURLOPT_RETURNTRANSFER, 1);
@@ -22,15 +22,17 @@ $authHttpCode = curl_getinfo($authCh, CURLINFO_HTTP_CODE);
 curl_close($authCh);
 
 if ($authHttpCode != 200) {
-    die(json_encode(array("error" => "Auth failed", "response" => json_decode($authResponse, true))));
+    die(json_encode(array("error" => "Auth failed", "code" => $authHttpCode)));
 }
 
 $authData = json_decode($authResponse, true);
-if (!isset($authData['access_token'])) {
-    die(json_encode(array("error" => "No token received", "response" => $authData)));
+
+// Fix: Token is in data[0].access_token
+if (!isset($authData['data'][0]['access_token'])) {
+    die(json_encode(array("error" => "No token in response", "response" => $authData)));
 }
 
-$accessToken = $authData['access_token'];
+$accessToken = $authData['data'][0]['access_token'];
 
 // Step 2: Fetch products using the token
 $ch = curl_init();
@@ -56,6 +58,6 @@ if ($httpcode == 200) {
         echo $response;
     }
 } else {
-    echo json_encode(array("error" => "Products fetch failed", "http_code" => $httpcode));
+    echo json_encode(array("error" => "Products fetch failed", "http_code" => $httpcode, "response" => json_decode($response, true)));
 }
 ?>
