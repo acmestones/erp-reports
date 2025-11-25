0b81b89de04fea2ad3daf8de87cb5697a1067c42
acmestones <marblehouse@gmail.com>
2025-11-25 11:37:14 +0530

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$testFile = __DIR__ . '/test_write.txt';
-if (file_put_contents($testFile, "test " . date('c')) !== false) {
-    echo "Write successful: $testFile";
-} else {
-    echo "Write failed: $testFile";
-}
