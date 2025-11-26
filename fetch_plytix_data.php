b6b94f71cd8eff72de611da8aecdfe844dc0807c
acmestones <marblehouse@gmail.com>
2025-11-26 14:09:59 +0530

    Update fetch_plytix_data.php

--- fetch_plytix_data.php
+++ fetch_plytix_data.php
@@ -18,7 +18,7 @@ error_log("=== PHP START: " . date('Y-m-d H:i:s') . " ===");
 
 $cacheTime = 3600;
 $limit = 25;
-$maxPages = 1;
+$maxPages = 1;  // Only fetch page 1 for now (25 products)
 
 $currentTime = time();
 if (file_exists($cacheFile) && ($currentTime - filemtime($cacheFile) < $cacheTime)) {
@@ -60,20 +60,50 @@ $authData = json_decode($authResponse, true);
 $accessToken = $authData['data'][0]['access_token'];
 $callCount = 1;
 
+// Function to fetch asset URL by ID
+function getAssetUrl($assetId, $accessToken) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://pim.plytix.com/api/v1/assets/" . $assetId);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $accessToken
+    ]);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    
+    $response = curl_exec($ch);
+    $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    if ($httpcode == 200) {
+        $assetData = json_decode($response, true);
+        // Try different response structures
+        if (isset($assetData['url'])) {
+            return $assetData['url'];
+        } elseif (isset($assetData['data']['url'])) {
+            return $assetData['data']['url'];
+        } elseif (isset($assetData['data'][0]['url'])) {
+            return $assetData['data'][0]['url'];
+        }
+    }
+    
+    error_log("Failed to get asset URL for ID: $assetId (HTTP $httpcode)");
+    return null;
+}
+
 $allProducts = [];
 $page = 1;
 
 while (true) {
     error_log("--- Fetching PAGE $page ---");
 
-    // CRITICAL FIX: Add sort order to make pagination work!
-$postData = [
+    // Don't specify attributes - let API return everything
+    $postData = [
         "limit" => $limit,
         "page" => $page,
         "sort" => [
             ["field" => "id", "order" => "asc"]
         ]
-        // DO NOT specify attributes - let API return all attribute values
     ];
 
     error_log("Request: " . json_encode($postData));
@@ -106,13 +136,50 @@ $postData = [
         $count = count($data['data']);
         error_log("Received $count products");
         
-        // Log first product on page 1
+        // Process each product to add asset URLs
+        error_log("Fetching asset URLs...");
+        foreach ($data['data'] as &$product) {
+            // Fetch thumbnail URL
+            if (isset($product['thumbnail']['id'])) {
+                $thumbnailId = $product['thumbnail']['id'];
+                error_log("Fetching thumbnail: $thumbnailId");
+                $thumbnailUrl = getAssetUrl($thumbnailId, $accessToken);
+                if ($thumbnailUrl) {
+                    $product['thumbnail_url'] = $thumbnailUrl;
+                    error_log("Got thumbnail URL: $thumbnailUrl");
+                }
+                $callCount++;
+                usleep(100000); // 100ms delay between asset calls
+            }
+            
+            // Fetch first 2 asset URLs to save API calls
+            if (isset($product['assets']) && is_array($product['assets'])) {
+                $assetUrls = [];
+                $assetCount = min(count($product['assets']), 2);
+                for ($i = 0; $i < $assetCount; $i++) {
+                    if (isset($product['assets'][$i]['id'])) {
+                        $assetId = $product['assets'][$i]['id'];
+                        $assetUrl = getAssetUrl($assetId, $accessToken);
+                        if ($assetUrl) {
+                            $assetUrls[] = $assetUrl;
+                        }
+                        $callCount++;
+                        usleep(100000);
+                    }
+                }
+                if (!empty($assetUrls)) {
+                    $product['asset_urls'] = $assetUrls;
+                }
+            }
+        }
+        unset($product); // Break reference
+        
+        // Log first product with URLs
         if ($page === 1) {
-            error_log("First product: " . json_encode($data['data'][0], JSON_PRETTY_PRINT));
+            error_log("First product with URLs: " . json_encode($data['data'][0], JSON_PRETTY_PRINT));
         }
         
         $allProducts = array_merge($allProducts, $data['data']);
-        
         error_log("Total products so far: " . count($allProducts));
 
         if ($count < $limit) {
