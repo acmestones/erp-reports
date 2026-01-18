7f5ed04abe5b260b895dcea084258a43309f14fd
acmestones <marblehouse@gmail.com>
2026-01-18 12:20:09 +0530

    Create update_video_representative.php

---
+++ update_video_representative.php
@@ -0,0 +1,27 @@
+<?php
+define('PHPWG_ROOT_PATH','../');
+include_once(PHPWG_ROOT_PATH.'include/common.inc.php');
+
+if (!is_admin()) {
+    die(json_encode(['success' => false, 'error' => 'Unauthorized']));
+}
+
+$video_id = isset($_POST['video_id']) ? intval($_POST['video_id']) : 0;
+
+if (!$video_id) {
+    die(json_encode(['success' => false, 'error' => 'Missing video_id']));
+}
+
+// Update database
+$query = 'UPDATE '.IMAGES_TABLE.' 
+          SET representative_ext = "jpg"
+          WHERE id = '.$video_id;
+pwg_query($query);
+
+// Clear cache for this image
+$query = 'DELETE FROM '.IMAGE_CATEGORY_TABLE.' WHERE image_id = '.$video_id;
+// Actually, we don't delete associations, just clear derivative cache
+invalidate_user_cache();
+
+echo json_encode(['success' => true, 'video_id' => $video_id]);
+?>
