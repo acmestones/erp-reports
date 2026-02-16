552abe5d51d9bb26e3244dd7594f49c593bcc099
acmestones <marblehouse@gmail.com>
2026-02-16 19:12:51 +0530

    Delete connection.php

--- connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("localhost","u165026639_maintenance","Calcutta!60","u165026639_maintenance");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connected successfully";
-?>
